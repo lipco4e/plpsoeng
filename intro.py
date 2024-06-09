e95daa9dab118bbd74e761b0d1bde926b967924e
lipco4e <inyemu@gmail.com>
2024-06-09 14:29:51 +0200

    added lessons on functions and loops

--- intro.py
+++ intro.py
@@ -8,9 +8,36 @@
 #     print("Incorrect user name")
 # else:
 #     print("Entry out of scope")
+#**************************************Control Structures**************************
+#List comprehension
 
-#:ist comprehension
+# sqr = [x**2 for x in range(10)]
+# print(sqr)
 
-sqr = [x**2 for x in range(10)]
-print(sqr)
+#for loops
+
+# for i in range(5):
+#     print(f"i is i")
+
+#while loop
+# count = 0
+
+# while count < 5:
+#     print(f"Count is {count}")
+#     count += 1
+
+#********************************************************Ends***********************************************************
+
+
+#*******************************************************Functions*******************************************************
+
+# def add(a, b):
+#     return a + b
+# results = add(10, 5)
+# print(results)
+
+# multiply = lambda x, y: x * y
+# print(multiply(3, 5))
+
+#********************************************************Ends***********************************************************
 
