6609e97c3e8d3574d20b86d7caa7847cb9819c6d
lipco4e <inyemu@gmail.com>
2024-06-09 13:31:07 +0200

    Added 2 files, Readme and intro to python file

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+This is my third commit
+How about trying now
+=======
+this
+>>>>>>> 10ca9aa6a688565c1edc140a4fde4ce48161bc42
---
+++ intro.py
@@ -0,0 +1,16 @@
+#This is on indentation
+
+# name = "Philip"
+
+# if name == "Philip":
+#     print("Hi "+name+" Welcome")
+# elif name != "Philip":
+#     print("Incorrect user name")
+# else:
+#     print("Entry out of scope")
+
+#:ist comprehension
+
+sqr = [x**2 for x in range(10)]
+print(sqr)
+
